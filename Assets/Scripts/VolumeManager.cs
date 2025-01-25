04b85dcd819d40051b709e008887b5ec4f31e4e2
Jorge <jorgezg0103@gmail.com>
2025-01-25 23:19:00 +0100

    Implemented volume manager

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -5,6 +5,36 @@ using UnityEngine.UI;
 public class VolumeManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer Audiomixer;
-    [SerializeField] private Slider VolumeSlider;
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Awake() {
+        if(PlayerPrefs.HasKey("MusicVolume")) {
+            SetSliders();
+            SetAudioMixer();
+        }
+    }
+
+    private void SetSliders() {
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicSlider");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXSlider");
+    }
+
+    private void SetAudioMixer() {
+        Audiomixer.SetFloat("SFXVolume", PlayerPrefs.GetFloat("SFXVolume"));
+        Audiomixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+    }
+
+    public void UpdateMusicVolume() {
+        Audiomixer.SetFloat("MusicVolume", MusicSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
+        Debug.Log(MusicSlider.value);
+    }
+
+    public void UpdateSFXVolume() {
+        Audiomixer.SetFloat("SFXVolume", SFXSlider.value);
+        PlayerPrefs.SetFloat("SFXVolume", SFXSlider.value);
+        Debug.Log(SFXSlider.value);
+    }
 
 }
