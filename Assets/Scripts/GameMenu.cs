5624b69dfa5b5a845172132a4827784fe247ee41
Jorge <jorgezg0103@gmail.com>
2025-01-25 21:59:21 +0100

    Added tutorial page in menu

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -3,7 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
+
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject Tutorial;
     public void LoadScene(int index) {
         SceneManager.LoadScene(index);
+    }
+
+    public void GoToTutorial() {
+        MainMenu.SetActive(false);
+        Tutorial.SetActive(true);
+    }
+
+    public void GoToMainMenu() {
+        MainMenu.SetActive(true);
+        Tutorial.SetActive(false);
     }
 }
