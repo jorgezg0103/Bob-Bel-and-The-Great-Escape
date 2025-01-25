d513c2198659ab07e7f1fce1afb9f4530530987f
Jorge <jorgezg0103@gmail.com>
2025-01-25 19:38:08 +0100

    Added menu buttons WIP

---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer Audiomixer;
+    [SerializeField] private Slider VolumeSlider;
+
+}
